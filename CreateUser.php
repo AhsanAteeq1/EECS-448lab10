c9814f424a8c7f46d013e69b9740311b72fdd0fc
AhsanAteeq1 <ahsanateeq7@gmail.com>
2021-12-10 00:01:11 -0600

    lab10

---
+++ CreateUser.php
@@ -0,0 +1,28 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "muhammadahsan", "Pi9eingu",
+"muhammadahsan");
+/* check connection */
+if ($mysqli->connect_errno) {
+ printf("Connect failed: %s\n", $mysqli->connect_error);
+ exit();
+}
+
+$user= $_POST["user"];
+if($user== "")
+{
+	echo "Not stored because the text field is empty";
+	exit();
+}
+
+$query = "INSERT INTO Users (user_id) VALUES ('".$user."')";
+if($mysqli->query($query))
+	{
+		echo "New user ".$user." stored successfully <br>";
+	}
+	else{
+		echo "The user ".$user." already exists<br>";
+	}
+
+
+$mysqli->close();
+?>
